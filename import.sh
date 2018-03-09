f7b85317142c72cf73d0d2e91ad261279b7b3e06
gongsi <you@example.com>
2018-03-09 03:06:35 -0500

    导入脚本

---
+++ import.sh
@@ -0,0 +1,22 @@
+#***********************************************
+#
+#      Filename: import.sh
+#
+#        Author: zwy - 865138769@qq.com
+#   Description: ---
+#        Create: 2018-03-09 02:44:34
+# Last Modified: 2018-03-09 02:44:34
+#***********************************************/
+
+#!/bin/bash
+base_dir=$(dirname $0)
+
+dir_name_bash="bash_profile"
+dir_name_vim="vim"
+
+file_name_bash="bash_profile"
+file_name_vim="vimrc"
+
+echo ${base_dir}/${dir_name_bash}/${file_name_bash}
+echo ${base_dir}/${dir_name_vim}/${file_name_vim}
+
